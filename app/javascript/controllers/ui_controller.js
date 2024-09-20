92c79d4265f4ab80cb67ffff9e31e599f639ed0a
Michael Vogl <michael.vogl@hey.com>
2024-09-20 15:02:44 +0200

    Small UI improvements

---
+++ app/javascript/controllers/ui_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [
+    "root",
+    "navigation",
+    "content",
+  ]
+
+  initialize() {}
+
+  toggleDarkMode(event) {
+    const currentTarget = event.currentTarget;
+    this.rootTarget.classList.toggle("dark");
+    [...currentTarget.children].forEach((element) => element.classList.toggle("hidden"));
+  }
+  expandNavigation(event) {
+    this.navigationTarget.classList.toggle("-translate-x-full")
+  }
+}
